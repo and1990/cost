b3be9636cb8c6a242bf899023dc253d9b6aa94af
and1990 <and19901109@163.com>
2013-12-28 17:26:28 +0800

    增加组实体、vo

---
+++ source/cost/cost-core/src/main/java/org/fire/cost/vo/GroupVO.java
@@ -0,0 +1,88 @@
+package org.fire.cost.vo;
+
+/**
+ * @author liutengfei
+ */
+public class GroupVO {
+    private Long groupId;
+    private String groupName;
+    private String userNames;
+    private Integer groupStatus;
+    private String createUser;
+    private String createTime;
+    private String modifyUser;
+    private String modifyTime;
+    private String groupRemark;
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getUserNames() {
+        return userNames;
+    }
+
+    public void setUserNames(String userNames) {
+        this.userNames = userNames;
+    }
+
+    public Integer getGroupStatus() {
+        return groupStatus;
+    }
+
+    public void setGroupStatus(Integer groupStatus) {
+        this.groupStatus = groupStatus;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getModifyUser() {
+        return modifyUser;
+    }
+
+    public void setModifyUser(String modifyUser) {
+        this.modifyUser = modifyUser;
+    }
+
+    public String getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(String modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public String getGroupRemark() {
+        return groupRemark;
+    }
+
+    public void setGroupRemark(String groupRemark) {
+        this.groupRemark = groupRemark;
+    }
+}
