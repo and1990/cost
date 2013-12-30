49dc737ead52896552454feb859520c259a19f25
and1990 <and19901109@163.com>
2013-12-30 20:39:28 +0800

    测试

---
+++ source/cost/cost-war/src/test/java/org/fire/cost/demo/JsonTest.java
@@ -0,0 +1,17 @@
+package org.fire.cost.demo;
+
+import org.junit.Test;
+
+/**
+ * 注释：
+ * 作者：liutengfei 【刘腾飞】
+ * 时间：13-12-29 上午10:20
+ */
+public class JsonTest
+{
+    @Test
+    public void jsonTest()
+    {
+        
+    }
+}
