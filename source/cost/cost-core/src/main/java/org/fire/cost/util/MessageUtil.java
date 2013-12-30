21064ca9890475da18e532b53cc8cc8dc143870a
and1990 <and19901109@163.com>
2013-12-30 12:13:20 +0800

    “组”CRUD操作

--- source/cost/cost-core/src/main/java/org/fire/cost/util/MessageUtil.java
+++ source/cost/cost-core/src/main/java/org/fire/cost/util/MessageUtil.java
@@ -6,23 +6,24 @@ import org.fire.cost.vo.Message;
 
 /**
  * 信息设置工具类
- * 
+ *
  * @author liutengfei
- * 
  */
-public class MessageUtil
-{
-	/**
-	 * 设置返回信息
-	 * 
-	 * @param statusEnum
-	 */
-	public static void setMessage(Message message, ResultEnum resultEnum, HttpStatusEnum statusEnum,
-			String exceptionMsg, Object data)
-	{
-		message.setResult(resultEnum.getCode());
-		message.setStatus(statusEnum.getCode());
-		message.setExceptionMsg(exceptionMsg);
-		message.setData(data);
-	}
+public class MessageUtil {
+    /**
+     * 设置返回信息
+     *
+     * @param message message信息
+     * @param resultEnum 返回结果枚举类
+     * @param statusEnum 返回状态枚举类
+     * @param descMsg 描述信息
+     * @param data 返回数据
+     */
+    public static void setMessage(Message message, ResultEnum resultEnum, HttpStatusEnum statusEnum,
+                                  String descMsg, Object data) {
+        message.setResult(resultEnum.getCode());
+        message.setStatus(statusEnum.getCode());
+        message.setDescMsg(descMsg);
+        message.setData(data);
+    }
 }
--- source/cost/cost-war/src/main/java/org/fire/cost/action/GroupAction.java
+++ source/cost/cost-war/src/main/java/org/fire/cost/action/GroupAction.java
@@ -1,10 +1,16 @@
 package org.fire.cost.action;
 
+import org.fire.cost.enums.HttpStatusEnum;
+import org.fire.cost.enums.ResultEnum;
+import org.fire.cost.service.GroupService;
+import org.fire.cost.util.MessageUtil;
 import org.fire.cost.vo.GroupVO;
 import org.fire.cost.vo.Message;
 import org.springframework.stereotype.Controller;
 
+import javax.annotation.Resource;
 import javax.ws.rs.Path;
+import java.util.List;
 
 /**
  * “组”Action
@@ -13,18 +19,29 @@ import javax.ws.rs.Path;
  */
 @Controller
 @Path("group")
-public class GroupAction
-{
+public class GroupAction {
+    @Resource
+    private GroupService groupService;
+
     /**
      * 根据过滤条件查询“组”数据
      *
      * @param vo 客户端传递的过滤数据
      * @return
      */
-    public Message getGroupByFilter(GroupVO vo)
-    {
-        return null;
-        //TODO
+    public Message getGroupByFilter(GroupVO vo) {
+        Message message = new Message();
+        if (vo == null) {
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Warn, "请求数据不能为空", null);
+            return message;
+        }
+        try {
+            List<GroupVO> voList = groupService.getGroupByFilter(vo);
+            MessageUtil.setMessage(message, ResultEnum.Success, HttpStatusEnum.Success, "查询成功", voList);
+        } catch (Exception e) {
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.ServerError, e.getMessage(), null);
+        }
+        return message;
     }
 
     /**
@@ -33,10 +50,22 @@ public class GroupAction
      * @param vo 客户端传递的数据
      * @return
      */
-    public Message addGroup(GroupVO vo)
-    {
-        return null;
-        //TODO
+    public Message addGroup(GroupVO vo) {
+        Message message = new Message();
+        if (vo == null) {
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Warn, "请求数据不能为空", null);
+            return message;
+        }
+        try {
+            if (groupService.addGroup(vo)) {
+                MessageUtil.setMessage(message, ResultEnum.Success, HttpStatusEnum.Success, "增加失败", null);
+            } else {
+                MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Success, "增加成功", null);
+            }
+        } catch (Exception e) {
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.ServerError, e.getMessage(), null);
+        }
+        return message;
     }
 
     /**
@@ -45,10 +74,22 @@ public class GroupAction
      * @param vo 客户端传递的数据
      * @return
      */
-    public Message updateGroup(GroupVO vo)
-    {
-        return null;
-        //TODO
+    public Message updateGroup(GroupVO vo) {
+        Message message = new Message();
+        if (vo == null) {
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Warn, "请求数据不能为空", null);
+            return message;
+        }
+        try {
+            if (groupService.updateGroup(vo)) {
+                MessageUtil.setMessage(message, ResultEnum.Success, HttpStatusEnum.Success, "更新成功", null);
+            } else {
+                MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Success, "更新失败", null);
+            }
+        } catch (Exception e) {
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.ServerError, e.getMessage(), null);
+        }
+        return message;
     }
 
     /**
@@ -57,10 +98,22 @@ public class GroupAction
      * @param groupId 组id
      * @return
      */
-    public Message deleteGroup(Long groupId)
-    {
-        return null;
-        //TODO
+    public Message deleteGroup(Long groupId) {
+        Message message = new Message();
+        if (groupId == null || groupId == 0) {
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Warn, "请求数据不正确", null);
+            return message;
+        }
+        try {
+            if (groupService.deleteGroup(groupId)) {
+                MessageUtil.setMessage(message, ResultEnum.Success, HttpStatusEnum.Success, "删除成功", null);
+            } else {
+                MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Success, "删除失败", null);
+            }
+        } catch (Exception e) {
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.ServerError, e.getMessage(), null);
+        }
+        return message;
     }
 
 }
