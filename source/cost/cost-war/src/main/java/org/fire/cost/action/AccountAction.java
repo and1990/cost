b475e05a8c38ac63fd7346f855b2da664c53324e
and1990 <and19901109@163.com>
2014-01-17 17:31:04 +0800

    添加文件上传

--- source/cost/cost-war/src/main/java/org/fire/cost/action/AccountAction.java
+++ source/cost/cost-war/src/main/java/org/fire/cost/action/AccountAction.java
@@ -1,14 +1,7 @@
 package org.fire.cost.action;
 
-import java.util.List;
-
-import javax.annotation.Resource;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
 import org.fire.cost.enums.HttpStatusEnum;
 import org.fire.cost.enums.ResultEnum;
 import org.fire.cost.service.AccountService;
@@ -17,144 +10,169 @@ import org.fire.cost.vo.AccountVO;
 import org.fire.cost.vo.Message;
 import org.springframework.stereotype.Controller;
 
+import javax.annotation.Resource;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
 /**
  * 账单信息表
- * 
+ *
  * @author liutengfei
- * 
  */
 @Controller
 @Path("account")
 public class AccountAction
 {
 
-	@Resource
-	private AccountService accountService;
+    @Resource
+    private AccountService accountService;
 
-	/**
-	 * 根据过滤条件查询账单信息
-	 * 
-	 * @param vo
-	 * @return
-	 */
-	@POST
-	@Path("getAccountByFilter")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Message getAccountByFilter(AccountVO vo)
-	{
-		Message message = new Message();
-		if (vo == null)
-		{
-			MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Warn, "请求数据不能为空", null);
-			return message;
-		}
-		try
-		{
-			List<AccountVO> voList = accountService.getAccountByFilter(vo);
-			MessageUtil.setMessage(message, ResultEnum.Success, HttpStatusEnum.Success, null, voList);
-		} catch (Exception e)
-		{
-			e.printStackTrace();
-			MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.ServerError, e.getMessage(), null);
-		}
-		return message;
-	}
+    /**
+     * 根据过滤条件查询账单信息
+     *
+     * @param vo
+     * @return
+     */
+    @POST
+    @Path("getAccountByFilter")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Message getAccountByFilter(AccountVO vo)
+    {
+        Message message = new Message();
+        if (vo == null)
+        {
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Warn, "请求数据不能为空", null);
+            return message;
+        }
+        try
+        {
+            List<AccountVO> voList = accountService.getAccountByFilter(vo);
+            MessageUtil.setMessage(message, ResultEnum.Success, HttpStatusEnum.Success, null, voList);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.ServerError, e.getMessage(), null);
+        }
+        return message;
+    }
 
-	/**
-	 * 增加账单
-	 * 
-	 * @param vo
-	 * @return
-	 */
-	@POST
-	@Path("addAccount")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Message addAccount(AccountVO vo)
-	{
-		Message message = new Message();
-		if (vo == null)
-		{
-			MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Warn, "请求数据不能为空", null);
-			return message;
-		}
-		try
-		{
-			if (accountService.addAccount(vo))
-			{
-				MessageUtil.setMessage(message, ResultEnum.Success, HttpStatusEnum.Success, null, null);
-			}
-		} catch (Exception e)
-		{
-			e.printStackTrace();
-			MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.ServerError, e.getMessage(), null);
-		}
-		return message;
-	}
+    /**
+     * 增加账单
+     *
+     * @param vo
+     * @return
+     */
+    @POST
+    @Path("addAccount")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Message addAccount(AccountVO vo)
+    {
+        Message message = new Message();
+        if (vo == null)
+        {
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Warn, "请求数据不能为空", null);
+            return message;
+        }
+        try
+        {
+            if (accountService.addAccount(vo))
+            {
+                MessageUtil.setMessage(message, ResultEnum.Success, HttpStatusEnum.Success, null, null);
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.ServerError, e.getMessage(), null);
+        }
+        return message;
+    }
 
-	/**
-	 * 修改账单信息
-	 * 
-	 * @param vo
-	 * @return
-	 */
-	@POST
-	@Path("modifyAccount")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Message modifyAccount(AccountVO vo)
-	{
-		Message message = new Message();
-		if (vo == null)
-		{
-			MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Warn, "请求数据不能为空", null);
-			return message;
-		}
-		try
-		{
-			if (accountService.modifyAccount(vo))
-			{
-				MessageUtil.setMessage(message, ResultEnum.Success, HttpStatusEnum.Success, null, null);
-			}
-		} catch (Exception e)
-		{
-			e.printStackTrace();
-			MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.ServerError, e.getMessage(), null);
-		}
-		return message;
-	}
+    /**
+     * 修改账单信息
+     *
+     * @param vo
+     * @return
+     */
+    @POST
+    @Path("modifyAccount")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Message modifyAccount(AccountVO vo)
+    {
+        Message message = new Message();
+        if (vo == null)
+        {
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Warn, "请求数据不能为空", null);
+            return message;
+        }
+        try
+        {
+            if (accountService.modifyAccount(vo))
+            {
+                MessageUtil.setMessage(message, ResultEnum.Success, HttpStatusEnum.Success, null, null);
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.ServerError, e.getMessage(), null);
+        }
+        return message;
+    }
 
-	/**
-	 * 删除账单信息
-	 * 
-	 * @param vo
-	 * @return
-	 */
-	@POST
-	@Path("deleteAccount")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Message deleteAccount(AccountVO vo)
-	{
-		Message message = new Message();
-		if (vo == null)
-		{
-			MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Warn, "请求数据不能为空", null);
-			return message;
-		}
-		try
-		{
-			if (accountService.deleteAccount(vo.getAccountId()))
-			{
-				MessageUtil.setMessage(message, ResultEnum.Success, HttpStatusEnum.Success, null, null);
-			}
-		} catch (Exception e)
-		{
-			e.printStackTrace();
-			MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.ServerError, e.getMessage(), null);
-		}
-		return message;
-	}
+    /**
+     * 删除账单信息
+     *
+     * @param vo
+     * @return
+     */
+    @POST
+    @Path("deleteAccount")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Message deleteAccount(AccountVO vo)
+    {
+        Message message = new Message();
+        if (vo == null)
+        {
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.Warn, "请求数据不能为空", null);
+            return message;
+        }
+        try
+        {
+            if (accountService.deleteAccount(vo.getAccountId()))
+            {
+                MessageUtil.setMessage(message, ResultEnum.Success, HttpStatusEnum.Success, null, null);
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            MessageUtil.setMessage(message, ResultEnum.Fail, HttpStatusEnum.ServerError, e.getMessage(), null);
+        }
+        return message;
+    }
+
+    /**
+     * 文件上传
+     *
+     * @param vo
+     * @param request
+     * @param response
+     * @return
+     */
+    @POST
+    @Path("fileUpload")
+    public Message fileUpload(AccountVO vo, @Context HttpRequest request, @Context HttpResponse response)
+    {
+        Message message = new Message();
+        return message;
+    }
+
 
 }
--- source/cost/cost-war/src/main/webapp/index.jsp
+++ source/cost/cost-war/src/main/webapp/index.jsp
@@ -1,5 +1,49 @@
+<%@ page language="java" contentType="text/html; charset=utf8"
+         pageEncoding="utf8"%>
+<%
+	String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort() + request.getContextPath();
+%>
 <html>
-<body>
-<h2>Hello World!</h2>
-</body>
+	<head>
+		<title>file upload</title>
+		<link rel="stylesheet" type="text/css" href="<%=basePath%>/js/uploadify/uploadify.css">
+		<script type="text/javascript" src="<%=basePath%>/easy-ui/jquery.min.js"></script>
+		<script type="text/javascript" src="<%=basePath%>/js/uploadify/jquery.uploadify.min.js"></script>
+		<style type="text/css">
+			.uploadify-button {
+
+				text-decoration:underline;
+				color:blue
+			}
+			.uploadify:hover .uploadify-button {
+
+				text-decoration:underline;
+				color:green
+			}
+		</style>
+		<script type="text/javascript">
+			$(function()
+			{
+				$('#file_upload').uploadify({
+					'swf'      : '<%=basePath%>/js/uploadify/uploadify.swf',
+					'uploader' : '<%=basePath%>/account/fileUpload',
+					'cancelImg' : '<%=basePath%>/js/uploadify/uploadify-cancel.png',
+					'buttonText' : '上传',
+					'auto':true,
+					'removeCompleted': false,
+					'fileTypeDesc': '选择文件',
+					'fileTypeExts': '*.gif; *.jpg; *.png',
+					'method' : 'post',
+					'formData':{'key':'value'},
+					'onUploadSuccess' : function(file,data,response){
+						alert("save data : " + data);
+					}
+				});
+			});
+		</script>
+	</head>
+	<body>
+		<input type="file" name="file_upload" id="file_upload" />
+		<a href="javascript:$('#file_upload').uploadify('upload','*')">Upload Files</a>
+	</body>
 </html>
