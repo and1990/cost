f83f30db5cf6d79eaf2140b2dcc8d46614f4a54e
and1990 <and19901109@163.com>
2014-05-05 13:52:27 +0800

    修改sql脚本

--- doc/需求文档/cost.sql
+++ doc/需求文档/cost.sql
@@ -1,71 +1,60 @@
-DROP DATABASE IF EXISTS cost;
+/*
+Navicat MySQL Data Transfer
 
-CREATE DATABASE cost DEFAULT CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+Source Server         : localhost
+Source Server Version : 50513
+Source Host           : localhost:3306
+Source Database       : cost_shasha
 
-USE cost;
+Target Server Type    : MYSQL
+Target Server Version : 50513
+File Encoding         : 65001
 
-DROP TABLE IF EXISTS cost_user;
+Date: 2014-05-05 10:28:08
+*/
 
-CREATE TABLE cost_user(
-user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-user_name VARCHAR(20) NOT NULL,
-password VARCHAR(20) NOT NULL,
-login_name VARCHAR(20) NOT NULL,
-user_age TINYINT(3) NOT NULL,
-user_address VARCHAR(150) NOT NULL,
-user_email VARCHAR(50) NOT NULL,
-user_image VARCHAR(100),
-user_status TINYINT(1) NOT NULL DEFAULT 2 COMMENT "1：不可用；2：可用",
-is_admin TINYINT(1) NOT NULL DEFAULT 1 COMMENT "1：普通用户；2：管理员",
-login_time TIMESTAMP,
-create_user VARCHAR(20),
-create_time TIMESTAMP,
-modify_user VARCHAR(20),
-modify_time TIMESTAMP,
-user_remark VARCHAR(200)
-)engine=InnoDB DEFAULT CHARSET=UTF8 AUTO_INCREMENT=1;
+SET FOREIGN_KEY_CHECKS=0;
 
-DROP TABLE IF EXISTS cost_account;
+-- ----------------------------
+-- Table structure for cost_account
+-- ----------------------------
+DROP TABLE IF EXISTS `cost_account`;
+CREATE TABLE `cost_account` (
+  `account_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `account_money` decimal(10,2) NOT NULL,
+  `account_time` date NOT NULL,
+  `account_type` int(1) NOT NULL DEFAULT '1' COMMENT '1：食物；2：娱乐：3：交通',
+  `account_status` int(1) NOT NULL DEFAULT '1' COMMENT '1：未审批；2：已审批',
+  `create_user` varchar(20) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT '0000-00-00 00:00:00',
+  `modify_user` varchar(20) DEFAULT NULL,
+  `modify_time` timestamp NULL DEFAULT '0000-00-00 00:00:00',
+  `account_remark` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`account_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `cost_account_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `cost_user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8;
 
-CREATE TABLE cost_account(
-account_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-user_id BIGINT NOT NULL,
-account_money DOUBLE NOT NULL,
-account_time DATE NOT NULL,
-account_type TINYINT(1) NOT NULL,
-approve_time TIMESTAMP,
-is_approve TINYINT(1) NOT NULL DEFAULT 1 COMMENT "1：未审批；2：已审批",
-account_partner VARCHAR(100),
-account_accessory VARCHAR(100),
-create_user VARCHAR(20),
-create_time TIMESTAMP,
-modify_user VARCHAR(20),
-modify_time TIMESTAMP,
-account_remark VARCHAR(200),
-FOREIGN KEY(user_id) REFERENCES cost_user(user_id)
-)engine=InnoDB DEFAULT CHARSET=UTF8 AUTO_INCREMENT=1;
-
-CREATE TABLE cost_group(
-  group_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  group_name VARCHAR(20) NOT NULL,
-  user_ids VARCHAR(200) NOT NULL,
-  group_status TINYINT(1) NOT NULL DEFAULT 1 COMMENT '1：不可用；2：可用',
-  create_user VARCHAR(20),
-  create_time TIMESTAMP,
-  modify_user VARCHAR(20),
-  modify_time TIMESTAMP,
-  group_remark varchar(200) DEFAULT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8 AUTO_INCREMENT=1;
-
-CREATE TABLE cost_group_account(
-  group_account_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  group_id BIGINT NOT NULL,
-  account_money DOUBLE NOT NULL,
-  start_date DATE NOT NULL,
-  end_date DATE NOT NULL,
-  is_over TINYINT(1) NOT NULL DEFAULT 1 COMMENT "1:未完结；2：已完结",
-  create_user VARCHAR(20),
-  create_time TIMESTAMP,
-  group_account_remark varchar(200) DEFAULT NULL,
-  FOREIGN KEY(group_id) REFERENCES cost_group(group_id)
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8 AUTO_INCREMENT=1;
+-- ----------------------------
+-- Table structure for cost_user
+-- ----------------------------
+DROP TABLE IF EXISTS `cost_user`;
+CREATE TABLE `cost_user` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(20) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `login_name` varchar(20) NOT NULL,
+  `user_age` int(3) NOT NULL,
+  `user_address` varchar(150) DEFAULT NULL,
+  `user_email` varchar(50) DEFAULT NULL,
+  `user_status` int(1) NOT NULL DEFAULT '2' COMMENT '1：不可用；2：可用',
+  `user_type` int(1) NOT NULL DEFAULT '1' COMMENT '1：普通用户；2：管理员',
+  `login_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_user` varchar(20) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `modify_user` varchar(20) DEFAULT NULL,
+  `modify_time` timestamp NULL DEFAULT NULL,
+  `user_remark` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
