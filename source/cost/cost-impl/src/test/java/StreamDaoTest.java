d37788028155273956fbfff04c47157a2dc1f5cb
and1990 <and19901109@163.com>
2014-05-07 18:07:24 +0800

    增加单元测试

---
+++ source/cost/cost-impl/src/test/java/StreamDaoTest.java
@@ -0,0 +1,30 @@
+import junit.framework.Assert;
+import org.fire.cost.dao.StreamDao;
+import org.fire.cost.domain.Stream;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 注释：
+ * 时间：2014年05月07日 下午6:01
+ * 作者：刘腾飞[liutengfei]
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = "classpath*:applicationContext.xml")
+public class StreamDaoTest {
+    @Resource
+    private StreamDao streamDao;
+
+    @Test
+    public void getStreamData() {
+        List<Stream> streamList = streamDao.findStreamByYear(2014);
+        Assert.assertNotNull(streamList);
+        System.out.println(streamList.size());
+    }
+
+}
